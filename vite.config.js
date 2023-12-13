d90757cc3c6deb011505d750553020a302226100
sidiraos <raossidi@gmail.com>
2023-12-13 10:23:49 +0300

    fixed  deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	build: {
-		base: '/Air-quality-App/', // Base URL pour les ressources statiques
+		// base: '/Air-quality-App/', // Base URL pour les ressources statiques
 		sourcemap: true, // Générer des sourcemaps
 		outDir: 'dist', // Répertoire de sortie pour la construction
 		assetsDir: 'assets', // Répertoire des ressources statiques
